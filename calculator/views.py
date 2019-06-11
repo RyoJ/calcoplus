a591bc7a92ee2a40f8b0a10f19751d60e19dab62
EC2 Default User <ec2-user@ip-172-31-31-202.us-east-2.compute.internal>
2019-06-11 23:15:59 +0000

    first my app

--- calculator/views.py
+++ calculator/views.py
@@ -8,11 +8,23 @@ def index(request):
     return render(request, 'index.html')
 
 def calc(request):
-    #val1 = int(request.POST['val1'])
-    val1=100
-    val2 = int(request.POST['val2'])
-    answer = val1 + val2
+    with open('/home/ec2-user/environment/django_projects/calculator/hp.txt','r') as f:
+        rhp = f.read()
+        #print(rhp) #読み込む時点ではphpより大きい。計算する時点では
+    
+    damage = int(request.POST['val2'])
+
+    with open('/home/ec2-user/environment/django_projects/calculator/hp.txt','w+') as f:
+        php = int(rhp) - int(damage)
+        #print(php)
+        f.write(str(php)) #事後の（新しい）hpが常に記録される
     context = {
-        'answer': answer,
+    'rhp': rhp,
+    'php': php,
     }
     return render(request, 'index.html', context)
+    
+'''def some_view(request):
+    if request.method == 'POST':
+        if 'button_1' in request.POST:
+            php = int(php) - 10'''
--- django_projects/settings.py
+++ django_projects/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '3.14.6.89',
+    '*',
     ]
 
 
